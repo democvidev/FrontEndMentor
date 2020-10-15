fa0c15f9d102117bddf0175c3be6cab0d1421c13
democvidev <cnvvc_vlad@mail.ru>
2020-10-15 21:36:11 +0200

    add javascript code

--- faq-accordion-card-main/public/js/script.js
+++ faq-accordion-card-main/public/js/script.js
@@ -1,23 +1,51 @@
-const cardData = [
+const answersData = [
     {
-        question: 'How many team members can I invite?',
-        answer: 'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.'
+        0: 'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.'
     },
     {
-        question: 'What is the maximum file upload size?',
-        answer: 'more than 2GB. All files in your account must fit your allotted storage space.'
+        1: 'No more than 2GB. All files in your account must fit your allotted storage space.'
     },
     {
-        question: 'How do I reset my password?',
-        answer: 'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.'
+        2: 'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.'
     },
     {
-        question: 'Can I cancel my subscription?',
-        answer: 'Yes! Send us a message and we’ll process your request no questions asked.'
+        3: 'Yes! Send us a message and we’ll process your request no questions asked.'
     },
     {
-        question: 'Do you provide additional support?',
-        answer: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.'
+        4: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.'
     }
 ];
+
+const textElements = document.querySelectorAll('.card-list__text--display');
+const blocEl = document.getElementById('bloc');
+
+showAnswer();
+
+function showAnswer() {
+    textElements.forEach((questEl) => {
+        questEl.onclick = function changeContent() {
+
+            const spanEl = questEl.children[0].id;
+            questEl.classList.toggle('bold');
+            questEl.children[1].classList.toggle('rotate');
+
+            if (questEl.nextElementSibling == null) {
+                const answer = document.createElement('p');
+                answer.classList.toggle('card-list__text--hidden');
+                answer.innerHTML = `${answersData[spanEl][spanEl]}`;
+                questEl.parentNode.appendChild(answer);
+            } else {
+                questEl.nextElementSibling.remove();
+            }
+        };
+
+        questEl.onmouseover = function moveBloc() {
+            blocEl.classList.toggle('move');
+        }
+    });
+}
+
+
+
+
 
